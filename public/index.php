cde5449bf3718fe95546f60610a421ff2d203de7
mat414748 <74719399+mat414748@users.noreply.github.com>
2022-10-24 11:37:42 +0200

    Change test

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use ReallySimpleJWT\Token;
-
+//Testich
 require __DIR__ . "/../vendor/autoload.php";
 require "model/db_functions.php";
 require "model/config.php";
